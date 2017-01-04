c9e2c94d0a6c9b169dbc62e5ec4dff160212bc66
biz123 <sprowl@palantir.net>
2017-01-04 13:07:47 -0800

    adds page piling POC

--- styleguide/src/content/assets/js/site.js
+++ styleguide/src/content/assets/js/site.js
@@ -13,7 +13,30 @@
 
   Drupal.behaviors.site = {
     attach: function (context, settings) {
-      console.log('Hello World!');
+      $('#pagepiling').pagepiling({
+        menu: null,
+        direction: 'vertical',
+        verticalCentered: false,
+        sectionsColor: [],
+        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+        scrollingSpeed: 700,
+        easing: 'swing',
+        loopBottom: false,
+        loopTop: false,
+        css3: true,
+        navigation: false,
+        normalScrollElements: null,
+        normalScrollElementTouchThreshold: 5,
+        touchSensitivity: 5,
+        keyboardScrolling: true,
+        sectionSelector: '.section',
+        animateAnchor: false,
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+      });
     }
   };
 
