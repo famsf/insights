7fa9f42b74571b4fd65b2f709789c60cb37ae94b
biz123 <sprowl@palantir.net>
2017-01-26 13:07:08 -0800

    adds political unrest section

--- styleguide/src/content/assets/js/site.js
+++ styleguide/src/content/assets/js/site.js
@@ -27,7 +27,7 @@
         direction: 'vertical',
         verticalCentered: false,
         sectionsColor: [],
-        anchors: ['title', 'introduction', 'contending-with-convention', 'see-the-forest-through-the-trees', 'ambition-and-risk', 'paris-modernity-and-leisure', 'monet-and-the-sea', 'camille-monet-the-known-and-the-unknown', 'lastPage'],
+        anchors: ['title', 'introduction', 'contending-with-convention', 'see-the-forest-through-the-trees', 'ambition-and-risk', 'paris-modernity-and-leisure', 'monet-and-the-sea', 'camille-monet-the-known-and-the-unknown', 'political-unrest', 'lastPage'],
         scrollingSpeed: 700,
         easing: 'swing',
         loopBottom: false,
