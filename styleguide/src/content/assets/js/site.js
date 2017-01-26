3212be9e2413ceaeb2cc0329127ccf30f246cdd0
biz123 <sprowl@palantir.net>
2017-01-26 12:56:43 -0800

    adds Camille section text

--- styleguide/src/content/assets/js/site.js
+++ styleguide/src/content/assets/js/site.js
@@ -27,7 +27,7 @@
         direction: 'vertical',
         verticalCentered: false,
         sectionsColor: [],
-        anchors: ['title', 'introduction', 'contending-with-convention', 'see-the-forest-through-the-trees', 'ambition-and-risk', 'paris-modernity-and-leisure', 'lastPage'],
+        anchors: ['title', 'introduction', 'contending-with-convention', 'see-the-forest-through-the-trees', 'ambition-and-risk', 'paris-modernity-and-leisure', 'monet-and-the-sea', 'camille-monet-the-known-and-the-unknown', 'lastPage'],
         scrollingSpeed: 700,
         easing: 'swing',
         loopBottom: false,
