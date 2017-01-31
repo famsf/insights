70601506364d5156eb59016566f24ab27c074b74
Michellanne Li <li@palantir.net>
2017-01-31 13:24:29 -0600

    for audio component, adds -js to styles targeted by js.

--- styleguide/src/content/assets/js/audio.js
+++ styleguide/src/content/assets/js/audio.js
@@ -13,19 +13,19 @@
   Drupal.behaviors.audio_toggle = {
     attach: function(context, settings) {
       // Define audio play buttons
-      var audioButton = $('.audio__button');
+      var audioButton = $('.audio__button-js');
       // Function to play and pause audio and toggle button states.
       var buttonToggle = function(button) {
         // Find the audio source related to this button.
-        var audioSource = button.closest('.audio__wrapper').find('.audio__source');
+        var audioSource = button.closest('.audio__wrapper-js').find('.audio__source-js');
         // Preload audio.
         // @todo: This should probably happen more globally and when a section
         // loads as part of the lazy loader functionality.
         audioSource.get(0).preload = "auto";
         // Toggle the class.
         // Toggle the button text and audio.
-        var playButton = button.find('.icon--play');
-        var pauseButton = button.find('.icon--pause');
+        var playButton = button.find('.icon--play-js');
+        var pauseButton = button.find('.icon--pause-js');
         if (playButton.is(':visible')) {
           audioSource.get(0).play();
           playButton.hide(0);
