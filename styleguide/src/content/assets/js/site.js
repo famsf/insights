61f28c24930d29f49fb0043789222c9a592d32c6
biz123 <sprowl@palantir.net>
2017-02-02 15:48:05 -0800

    fixes loading on the non-demo parts of the styleguide.

--- styleguide/src/content/assets/js/site.js
+++ styleguide/src/content/assets/js/site.js
@@ -13,26 +13,97 @@
 
   Drupal.behaviors.site = {
     attach: function (context, settings) {
-      // Create an array of section IDs for the page card deck.
-      var sectionElements = $('.js-section');
-      var sectionIds = [];
-      function getSectionIds() {
-        for (var i = sectionElements.length - 1; i >= 0; i--) {
-          sectionIds.unshift($(sectionElements[i]).attr('id'));
-        }
-      }
-      getSectionIds();
 
-      // Start the slider
-      $.extend($.lazyLoadXT, {
-        autoInit: false,
-        edgeY:  10000,
-        visibleOnly: false
-      });
-      var lazyloadSection = function(section) {
-        $(section).find('img[data-src],div[data-bg]').lazyLoadXT();
-        console.log(section);
-      };
+      if ($('.js-pagepiling').length) {
+        // We are on a page that uses pagepiling.js.
+        // Create an array of section IDs for the pagepiling card deck.
+        // This is required to create hashtag sections.
+        var sectionElements = $('.js-section');
+        var sectionIds = [];
+        var getSectionIds = function () {
+          for (var i = sectionElements.length - 1; i >= 0; i--) {
+            sectionIds.unshift($(sectionElements[i]).attr('id'));
+          }
+        };
+        getSectionIds();
+
+        $.extend($.lazyLoadXT, {
+          autoInit: false,
+          edgeY:  10000,
+          visibleOnly: false
+        });
+        // Helper function so we can activate lazy load by section.
+        var lazyloadSection = function(section, complete) {
+          $(section).find('img[data-src],div[data-bg]').lazyLoadXT({oncomplete: complete});
+        };
+        // Initialize pagepiling.
+        $('.js-pagepiling').pagepiling({
+          menu: null,
+          direction: 'vertical',
+          verticalCentered: false,
+          sectionsColor: [],
+          anchors: sectionIds,
+          scrollingSpeed: 700,
+          easing: 'swing',
+          loopBottom: false,
+          loopTop: false,
+          css3: true,
+          navigation: false,
+          normalScrollElements: null,
+          normalScrollElementTouchThreshold: 5,
+          touchSensitivity: 5,
+          keyboardScrolling: true,
+          sectionSelector: '.js-section',
+          animateAnchor: false,
+
+          //events
+          onLeave: function(index, nextIndex, direction){
+            // Select the next active index section element by it's index.
+            var nextActiveSection = nextIndex - 1;
+            var sectionSelector = $('section.js-section').eq(nextActiveSection);
+            lazyloadSection(sectionSelector);
+            // Get all the animated item in the section.
+            var animatedItems = sectionSelector.find('*.animated');
+            var numberOfAnimatedItems = animatedItems.length;
+            // Function to animate things in the viewport.
+            function animateInSectionView() {
+              for (var i = 0; i < numberOfAnimatedItems; i++) {
+                if ($(animatedItems[i]).position().top < $(sectionSelector).height()){
+                  $(animatedItems[i]).addClass('go');
+                }
+              }
+            }
+            // Run animations when the new card is initially visible.
+            animateInSectionView();
+
+            // Trigger all animation in view on scroll.
+            sectionSelector.scroll(function () {
+              animateInSectionView();
+            });
+          },
+          afterLoad: function(anchorLink, index){},
+          afterRender: function(){
+            // Start lazy load on the first active section and hide the loader.
+            var firstSection = $('.js-section.active');
+            var hideLoader = function() {
+              $('.js-loading').fadeOut(200);
+            };
+            lazyloadSection(firstSection, hideLoader());
+          },
+        });
+        $('.js-next-page').click( function() {
+          $.fn.pagepiling.moveSectionDown();
+        });
+      } else {
+        // We are not using pagepiling so we should lazy load with auto init.
+        $.extend($.lazyLoadXT, {
+          autoInit: true,
+          visibleOnly: false,
+          selector: 'img[data-src],div[data-bg]'
+        });
+      }
+
+      // Start the word slider
       $('.js-wordslider').bxSlider({
         mode: 'fade',
         pager: false,
@@ -40,63 +111,6 @@
         speed: 2000,
         pause: 3000
 
-      });
-      $('.js-pagepiling').pagepiling({
-        menu: null,
-        direction: 'vertical',
-        verticalCentered: false,
-        sectionsColor: [],
-        anchors: sectionIds,
-        scrollingSpeed: 700,
-        easing: 'swing',
-        loopBottom: false,
-        loopTop: false,
-        css3: true,
-        navigation: false,
-        normalScrollElements: null,
-        normalScrollElementTouchThreshold: 5,
-        touchSensitivity: 5,
-        keyboardScrolling: true,
-        sectionSelector: '.js-section',
-        animateAnchor: false,
-
-        //events
-        onLeave: function(index, nextIndex, direction){
-          // Select the next active index section element by it's index.
-          var nextActiveSection = nextIndex - 1;
-          var sectionSelector = $('section.js-section').eq(nextActiveSection);
-          lazyloadSection(sectionSelector);
-          // Get all the animated item in the section.
-          var animatedItems = sectionSelector.find('*.animated');
-          var numberOfAnimatedItems = animatedItems.length;
-          // Function to animate things in the viewport.
-          function animateInSectionView() {
-            for (var i = 0; i < numberOfAnimatedItems; i++) {
-              if ($(animatedItems[i]).position().top < $(sectionSelector).height()){
-                $(animatedItems[i]).addClass('go');
-              }
-            }
-          }
-          // Run animations when the new card is initially visible.
-          animateInSectionView();
-
-          // Trigger all animation in view on scroll.
-          sectionSelector.scroll(function () {
-            animateInSectionView();
-          });
-        },
-        afterLoad: function(anchorLink, index){
-        },
-        afterRender: function(){
-          // Start lazy load on the first active section.
-          var firstSection = $('.js-section.active');
-          console.log(firstSection);
-          lazyloadSection(firstSection);
-          $('.js-loading').fadeOut(100);
-        },
-      });
-      $('.js-next-page').click( function() {
-        $.fn.pagepiling.moveSectionDown();
       });
     }
   };
