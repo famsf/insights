5dedec1368b7bd7e94e32815e4616e31d3d9b8df
Ashley Cyborski <cyborski@palantir.net>
2017-02-17 15:09:33 -0600

    Fix js up

--- styleguide/src/content/assets/js/audio-transcript.js
+++ styleguide/src/content/assets/js/audio-transcript.js
@@ -15,7 +15,7 @@
     attach: function(context, settings) {
       $('.js-audio-transcript--trigger', context).click(function(){
         // Define the target expandable div.
-        var targetDrawer = $(this).parents('audio-transcript').find('audio-transcript__drawer');
+        var targetDrawer = $(this).parents('.audio-transcript').find('.audio-transcript__drawer');
         targetDrawer.toggleClass('js-audio-transcript--hidden');
       });
     }
