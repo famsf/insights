17dcb08c0a5c0fc9060de92bf480cf1ccd610361
Thiago de Mello Bueno <thiago@madeofpeople.org>
2018-06-14 13:33:23 -0400

    FDS-10 -- better initialization, tied to background image loading

--- src/components/molecules/CoverPage/CoverPage.js
+++ src/components/molecules/CoverPage/CoverPage.js
@@ -1,40 +1,53 @@
+(function(fds, coverPage, window){
 
-document.addEventListener("DOMContentLoaded", function(){
+  document.addEventListener("DOMContentLoaded", function(){
 
-  /*
-   * Call the polyfill
-   *
-   * patternID : the unique ID of the SVG pattern
-   * patternURL : the URL to the background-image
-   * class : the css-class applied to the SVG
-   */
-
-  element.backgroundClipPolyfill({
-    'patternID' : 'mypattern',
-    // todp, put patternURL into a data-attribute
-    'patternURL' : 'https://3.bp.blogspot.com/-RE9D7tm8uVU/VvecOJZ5ddI/AAAAAAAAgqc/TUZpJwTFqH4TR7oG4J3GzuFhr1NOAuYJw/w1200-h630-p-k-no-nu/Lady%2Bwith%2BHat%2Band%2BFeather%2BBoa%2Bby%2BGustav%2BKlimt.jpg',
-    'class' : 'headline'
-  });
-
-
-  var coverPage = document.getElementById('CoverPage')
-  var container = coverPage.querySelector('.cover_lodaer_container');
-  var maskLoader = container.querySelector('.mask_loader')
-  var underlay = coverPage.querySelector('.underlay');
-
-  setTimeout(function() {
     /*
-     move initializationn probably to when the image is loaded, we should see what we can learn from
-     https://www.lullabot.com/articles/making-legacy-sites-more-performant-using-modern-frontend-techniques
-    */
-    maskLoader.classList.add('open')
+     * Call the polyfill
+     *
+     * patternID : the unique ID of the SVG pattern
+     * patternURL : the URL to the background-image
+     * class : the css-class applied to the SVG
+     */
+
+    element.backgroundClipPolyfill({
+      'patternID' : 'mypattern',
+      // todp, put patternURL into a data-attribute
+      'patternURL' : 'https://3.bp.blogspot.com/-RE9D7tm8uVU/VvecOJZ5ddI/AAAAAAAAgqc/TUZpJwTFqH4TR7oG4J3GzuFhr1NOAuYJw/w1200-h630-p-k-no-nu/Lady%2Bwith%2BHat%2Band%2BFeather%2BBoa%2Bby%2BGustav%2BKlimt.jpg',
+      'class' : 'headline'
+    });
+
+    fds.coverPageElement = document.getElementById('CoverPage')
+    fds.coverPage.initialize()
+  })
+
+  fds.coverPage.initialize = function() {
+    var imgSrc = fds.coverPageElement.getAttribute('data-bg-image')
+    var imgAlt = fds.coverPageElement.getAttribute('data-bg-image-alt')
+    var img = new Image()
+    img.setAttribute('alt', imgAlt)
+    img.setAttribute('src', imgSrc)
+    if(img.complete) {
+      fds.onCoverImageLoaded()
+    } else {
+      img.addEventListener('load', function() {
+        fds.onCoverImageLoaded()
+      })
+      img.addEventListener('error', function() {
+        console.log("Failed to load cover image")
+      })
+    }
+  }
+
+  fds.onCoverImageLoaded = function() {
+    console.log('onCoverImageLoaded')
+    fds.coverPageElement.classList.add('loaded')
     setTimeout(function() {
-      container.classList.add('fadeout')
+      fds.coverPageElement.classList.add('post_loaded')
       setTimeout(function(){
-        underlay.classList.add('to_top')
-        maskLoader.style.display = 'none'
-        container.style.display = 'none'
+        fds.coverPageElement.classList.add('initialized')
       }, 750)
     }, 1250)
-  }, 3000)
-})
+  }
+
+})(window.fds = window.fds || {}, window.fds.coverPage = window.fds.coverPage || {}, window);
