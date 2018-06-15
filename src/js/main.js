b2c3c19ece8435ce66f2e11f262eb952944f550d
Dustin Harrell <dustin@avidnewmedia.io>
2018-06-15 16:51:15 -0400

    fds-30 updates to owl js

--- src/js/main.js
+++ src/js/main.js
@@ -9,17 +9,15 @@ $(document).ready(function() {
   owl.owlCarousel({
     items: 2,
     merge: true,
-    loop: true,
+    loop: false,
     nav: true,
-    margin: 10,
+    margin: 0,
     mergeFit: true
   });
   
   owl.on('mousewheel', '.owl-stage', function (e) {
-    if (e.deltaY>0) {
+    if (e.deltaY > 0) {
       owl.trigger('next.owl');
-    } else {
-      owl.trigger('prev.owl');
     }
     e.preventDefault();
   });
