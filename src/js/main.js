b5211a8d2b16e754d1b4d4bd98c12c697f325ced
Dustin Harrell <dustin@avidnewmedia.io>
2018-06-18 12:59:49 -0400

    updates to horizontal image slider for demo

--- src/js/main.js
+++ src/js/main.js
@@ -17,13 +17,14 @@
     margin: 32,
     mergeFit: true
   });
-  
-  owl.on('mousewheel', '.owl-stage', function (e) {
-    if (e.deltaY > 0) {
-      owl.trigger('next.owl');
-    }
-    e.preventDefault();
-  });
+
+  // Prevents scrolling vertically until all slides have been seen.
+  // owl.on('mousewheel', '.owl-stage', function (e) {
+  //   if (e.deltaY > 0) {
+  //     owl.trigger('next.owl');
+  //   }
+  //   e.preventDefault();
+  // });
 
   fds.setStyle = function(el, obj) {
     el.style = Object.assign(el.style, obj)
