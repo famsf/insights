188c090bf80538442702cbf03990b49c61ee41b4
Hawkeye (Derek DeRaps) Tenderwolf <hawkeye@tenderwolf.io>
2018-06-21 19:51:14 -0400

    Second round of autofixing of JS via ESLint

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
-(function(fds, window, document, $ ) {
-
+(function (fds, window, document, $) {
   // Always use the smoothscroll polyfill, even in browsers with native support.
   window.__forceSmoothScrollPolyfill__ = true;
 
@@ -26,13 +25,13 @@
   //   e.preventDefault();
   // });
 
-  fds.setStyle = function(el, obj) {
-    el.style = Object.assign(el.style, obj)
-  }
+  fds.setStyle = function (el, obj) {
+    el.style = Object.assign(el.style, obj);
+  };
 
-  fds.getParentEl = function(el, selector) {
-    var elements = []
-    var ishaveselector = selector !== undefined
+  fds.getParentEl = function (el, selector) {
+    var elements = [];
+    var ishaveselector = selector !== undefined;
     while ((el = el.parentElement) !== null) {
       if (el.nodeType !== Node.ELEMENT_NODE) {
         continue;
@@ -42,7 +41,7 @@
       }
     }
     if (elements.length === 1) {
-      elements = elements[0]
+      elements = elements[0];
     }
     try {
       return elements;
@@ -50,78 +49,79 @@
     finally {
       elements = null;
     }
-  }
+  };
 
   $(document).ready(function () {
-
-    var win = window
-    var frameCount = 0
-    var calcFps = true
-    var scrollDir
-    if(calcFps) {
-      fds.targetFps = 60
-      fds.FpsInterval = 1000 / fds.targetFps
+    var win = window;
+    var frameCount = 0;
+    var calcFps = true;
+    var scrollDir;
+    if (calcFps) {
+      fds.targetFps = 60;
+      fds.FpsInterval = 1000 / fds.targetFps;
     }
     var st = 0;
     var wDim = {
       w: win.innerWidth,
       h: win.innerHeight
-    }
+    };
 
     if (!document.querySelector('.insights-app')) {
-      console.log('Bypassing main js loop in current context to allow for easier single component prototyping')
+      console.log('Bypassing main js loop in current context to allow for easier single component prototyping');
       return;
     }
 
-    fds.fpsEl = document.getElementById('fpsEl')
-    window.fds.pages.initialize('.container', '.page', '.top-bar')
-    window.fds.chapterNav.initialize('#chapter_nav', '.chapter', '.top-bar')
-    window.fds.topBar.initialize('topBar')
-    window.fds.pages.onScroll(0, 'down', win.innerHeight, true)
-    window.fds.chapterNav.onScroll()
+    fds.fpsEl = document.getElementById('fpsEl');
+    window.fds.pages.initialize('.container', '.page', '.top-bar');
+    window.fds.chapterNav.initialize('#chapter_nav', '.chapter', '.top-bar');
+    window.fds.topBar.initialize('topBar');
+    window.fds.pages.onScroll(0, 'down', win.innerHeight, true);
+    window.fds.chapterNav.onScroll();
     animate = function (newtime) {
       // console.log('»', newtime, )
       requestAnimationFrame(animate);
-      var elapsed, didResize, msPerFrame, oldWindowDim;
+      var elapsed,
+        didResize,
+        msPerFrame,
+        oldWindowDim;
       if (calcFps) {
-        elapsed =  newtime - then
-        didResize = false
-        msPerFrame = 0
+        elapsed = newtime - then;
+        didResize = false;
+        msPerFrame = 0;
       }
-      oldWindowDim = Object.assign({}, wDim)
+      oldWindowDim = Object.assign({}, wDim);
       wDim = {
         w: win.innerWidth,
         h: win.innerHeight
-      }
+      };
       if (wDim.w != oldWindowDim.w || wDim.h != oldWindowDim.h) {
-        didResize = true
+        didResize = true;
       }
       if (elapsed > fds.FpsInterval) {
-        var oldSt = st
-        st = window.poly.getScrollY()
-        var scrollDiff = st - oldSt
-        if( scrollDiff != 0 ) {
-          scrollDir = ( scrollDiff > 0 ) ? 'down' : 'up';
-          window.fds.pages.onScroll(st, scrollDir, wDim.h, didResize)
-          window.fds.chapterNav.onScroll()
+        var oldSt = st;
+        st = window.poly.getScrollY();
+        var scrollDiff = st - oldSt;
+        if (scrollDiff != 0) {
+          scrollDir = (scrollDiff > 0) ? 'down' : 'up';
+          window.fds.pages.onScroll(st, scrollDir, wDim.h, didResize);
+          window.fds.chapterNav.onScroll();
         }
       }
       if (calcFps) {
         var sinceStart = newtime - fds.startTime;
-        var currentFps = Math.round((1000 / (sinceStart / ++frameCount) * 100) * 0.01)
-        var curFrameTime = elapsed
-        var msPerFrame = Math.round(sinceStart/frameCount)
+        var currentFps = Math.round((1000 / (sinceStart / ++frameCount) * 100) * 0.01);
+        var curFrameTime = elapsed;
+        var msPerFrame = Math.round(sinceStart / frameCount);
         fds.fpsEl.innerHTML = currentFps + ' fps at roughly<br/>' + msPerFrame + ' ms/frame';
-        then = newtime - (elapsed % fds.FpsInterval)
+        then = newtime - (elapsed % fds.FpsInterval);
         sinceStart = currentFps = curFrameTime = msPerFrame = null;
       }
-    }
+    };
     if (calcFps) {
-      var then = window.performance.now()
-      fds.startTime = then
+      var then = window.performance.now();
+      fds.startTime = then;
     }
     requestAnimationFrame(animate);
-    then = elapsed = didResize = msPerFrame = oldWindowDim = scrollDir = null
-
-  })
-})( window.fds = window.fds || {}, window, document, jQuery);
+    then = elapsed = didResize = msPerFrame = oldWindowDim = scrollDir = null;
+  });
+}(window.fds = window.fds || {}, window, document, jQuery));
