0d92652340badb47e620fe94c83e5156dadb82ef
Angelo Porretta <angelo@kalamuna.com>
2018-10-03 14:22:52 -0400

    FDS-168. Zoom polish.

--- src/components/molecules/zoom-image--fullbleed/zoom-image--fullbleed.js
+++ src/components/molecules/zoom-image--fullbleed/zoom-image--fullbleed.js
@@ -4,5 +4,23 @@
 (function (window, $, ImageZoom) {
   $(document).ready(function () {
     var zoomedImages = new ImageZoom('img.img-zoom-drag', { maxZoom: 3, backgroundImageColor: '#000' });
+    $('.img-zoom-drag').mousemove(function (e) {
+      var offset = $(this).offset();
+      var finderBox = $(this).closest('.zoom-image--fullbleed').find('.finder-box');
+      var relX = e.pageX - offset.left;
+      var relY = e.pageY - offset.top;
+      var imgWidth = $(this).width();
+      var imgHeight = $(this).height();
+      var percentThroughWidth = (relX / imgWidth * 100) * .5;
+      var percentThroughHeight = (relY / imgHeight * 100)  * .5;
+      var windowWidth = $(window).width();
+      var windowHeight = $(window).height();
+      finderBox.css({
+        'top': percentThroughHeight + '%',
+        'left': percentThroughWidth + '%',
+        'height': windowHeight / 12 + 'px',
+        'width': windowWidth / 12 + 'px'
+      })
+    });
   });
 }(window, jQuery, ImageZoom));
