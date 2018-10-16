9b5ef7a0e76c48c1b81f1c0516d6ceba6d60fe5a
Angelo Porretta <angelo@kalamuna.com>
2018-10-16 11:26:02 -0400

    FDS-183. Putting the hub on its own page.

--- src/components/molecules/hub/hub.js
+++ src/components/molecules/hub/hub.js
@@ -1,65 +1,70 @@
 (function (document, window, $) {
-  var $hubs = $('.hub');
-  $(document).foundation();
+  $(document).ready(function () {
+    var $hubs = $('.frame > .hub');
+    $(document).foundation();
+    function createFilters($hub) {
+      var tagMap = {};
+      var $tags = $hub.find('div[data-tag-value]');
+      var colorScheme = 'teal';
+      $tags.each(function (i, e) {
+        tagMap[$(e).attr('data-tag-value')] = true;
+      });
+      Object.keys(tagMap).forEach(function (tag) {
+        $hub.find('.story-filter').append('<a href="#" aria-role="button" data-showing="true" class="' + colorScheme + '">' + tag + '</a>');
+        if (colorScheme === 'teal') colorScheme = 'blue';
+        else if (colorScheme === 'blue') colorScheme = 'black';
+        else colorScheme = 'teal';
+      });
+    }
 
-  function createFilters($hub) {
-    var tagMap = {};
-    var $tags = $hub.find('div[data-tag-value]');
-    var colorScheme = 'teal';
-    $tags.each(function (i, e) {
-      tagMap[$(e).attr('data-tag-value')] = true;
-    });
-    Object.keys(tagMap).forEach(function (tag) {
-      $hub.find('.story-filter').append('<a href="#" aria-role="button" data-showing="true" class="' + colorScheme + '">' + tag + '</a>');
-      if (colorScheme === 'teal') colorScheme = 'blue';
-      else if (colorScheme === 'blue') colorScheme = 'black';
-      else colorScheme = 'teal';
-    });
-  }
+    function highlightTitle($hub) {
+      var title = $hub.find('h1').text();
+      var lastWord = title.split(' ').pop();
+      var newTitle = title.replace(lastWord, '<span class="word-highlight">' + lastWord + '</span>');
+      $hub.find('h1').html(newTitle);
+    }
 
-  function highlightTitle($hub) {
-    var title = $hub.find('h1').text();
-    var lastWord = title.split(' ').pop();
-    var newTitle = title.replace(lastWord, '<span class="word-highlight">' + lastWord + '</span>');
-    $hub.find('h1').html(newTitle);
-  }
-
-  $hubs.each(function (i, e) {
-    var $hub = $(e);
-    var $storyFilter = $(e).find('.story-filter');
-    var flickityCarousel;
-    highlightTitle($hub);
-    createFilters($hub);
-    flickityCarousel = $storyFilter.flickity({
-      cellAlign: 'left',
-      contain: true,
-      draggable: true,
-      prevNextButtons: false,
-      pageDots: false
-    });
-    $hub.find('.story-filter a').on('click', function (event) {
-      event.preventDefault();
-    });
-    flickityCarousel.on('staticClick.flickity', function (event, pointer, cellElement, cellIndex) {
-      var tagText = $(cellElement).text();
-      var $stories = $hub.find('.hub-story');
-      if ($(cellElement).hasClass('filtered')) {
-        $(cellElement).removeClass('filtered');
-      }
-      else {
-        $(cellElement).addClass('filtered');
-      }
-      $stories.each(function (index, element) {
-        $(element).find('div[data-tag-value="' + tagText + '"]').css('display', 'none');
-        if ($(element).find('div[data-tag-value="' + tagText + '"]').length !== 0) {
-          if ($(cellElement).attr('data-showing') === 'true') {
-            $(element).fadeOut();
-            $(cellElement).attr('data-showing', 'false');
+    $hubs.each(function (i, e) {
+      var $hub = $(e);
+      var $storyFilter = $(e).find('.story-filter');
+      var flickityCarousel;
+      highlightTitle($hub);
+      createFilters($hub);
+      flickityCarousel = $storyFilter.flickity({
+        cellAlign: 'left',
+        contain: true,
+        draggable: true,
+        prevNextButtons: false,
+        pageDots: false
+      });
+      $hub.find('.story-filter a').on('click', function (event) {
+        event.preventDefault();
+      });
+      flickityCarousel.on('staticClick.flickity', function (event, pointer, cellElement, cellIndex) {
+        var tagText = $(cellElement).text();
+        var $stories = $hub.find('.hub-story');
+        if ($(cellElement).hasClass('filtered')) {
+          $(cellElement).removeClass('filtered');
+        }
+        else {
+          $(cellElement).addClass('filtered');
+        }
+        $stories.each(function (index, element) {
+          $(element).find('div[data-tag-value="' + tagText + '"]').css('display', 'none');
+          if ($(element).find('div[data-tag-value="' + tagText + '"]').length !== 0) {
+            if ($(cellElement).attr('data-showing') === 'true') {
+              $(element).fadeOut(200);
+            }
+            else {
+              $(element).fadeIn(200);
+            }
           }
-          else {
-            $(element).fadeIn();
-            $(cellElement).attr('data-showing', 'true');
-          }
+        });
+        if ($(cellElement).attr('data-showing') === 'true') {
+          $(cellElement).attr('data-showing', 'false');
+        }
+        else {
+          $(cellElement).attr('data-showing', 'true');
         }
       });
     });
---
+++ src/index.md
@@ -0,0 +1,71 @@
+---
+layout: "templates/pages/digital-story.html.twig"
+title: Hub
+tagline: From the de Young and Legion of Honor
+twitter_url: 'https://twitter.com/legionofhonor'
+facebook_url: 'https://www.facebook.com/LegionofHonor/'
+tickets_url: 'https://tickets.famsf.org/events/283/list'
+bodyId: "hub"
+chapters:
+
+################################################################################
+## Introduction ################################################################
+################################################################################
+
+- title: Introduction
+  numeral: A
+  pages:
+
+  # i
+  - components:
+    - template: "@molecules/hub/hub.html.twig"
+      featured_img_src: "https://www.fillmurray.com/1500/1200"
+      featured_img_alt: "Fubar"
+      is_on_view: true
+      featured_title: "This is a featured title"
+      featured_subtitle: "This is a featured subtitle"
+      featured_read_cta_text: "Read More"
+      featured_read_cta_link: "http://google.ca"
+      tickets_read_cta_text: "Buy Tickets"
+      tickets_read_cta_link: "http://google.ca"
+      stories:
+      - template: "@molecules/hub/hub--story.html.twig"
+        is_on_view: true
+        image_src: "https://insights.famsf.org/assets/images/delaunay.jpg"
+        image_alt: "Fubar"
+        title: "This is a story"
+        subtitle: "This is a story subtitle"
+        read_link: "https://google.ca"
+        tags: "hello,world,fubar"
+      - template: "@molecules/hub/hub--story.html.twig"
+        is_on_view: true
+        image_src: "https://insights.famsf.org/assets/images/delaunay.jpg"
+        image_alt: "Fubar"
+        title: "This is a story"
+        subtitle: "This is a story subtitle"
+        read_link: "https://google.ca"
+        tags: "hello,world,flubber"
+      - template: "@molecules/hub/hub--story.html.twig"
+        is_on_view: true
+        image_src: "https://insights.famsf.org/assets/images/delaunay.jpg"
+        image_alt: "Fubar"
+        title: "This is a story"
+        subtitle: "This is a story subtitle"
+        read_link: "https://google.ca"
+        tags: "hello,world,flubber"
+      - template: "@molecules/hub/hub--story.html.twig"
+        is_on_view: true
+        image_src: "https://insights.famsf.org/assets/images/delaunay.jpg"
+        image_alt: "Fubar"
+        title: "This is a story"
+        subtitle: "This is a story subtitle"
+        read_link: "https://google.ca"
+        tags: "hello,world,flubber"
+      - template: "@molecules/hub/hub--story.html.twig"
+        is_on_view: true
+        image_src: "https://insights.famsf.org/assets/images/delaunay.jpg"
+        image_alt: "Fubar"
+        title: "This is a story"
+        subtitle: "This is a story subtitle"
+        read_link: "https://google.ca"
+        tags: "hello,world,flubber"
