a6c434779513a335c5978d9cd17e01b9ba9e2a66
Angelo Porretta <angelo@kalamuna.com>
2018-10-26 14:04:52 -0700

    FDS-73. Adding random sort order and a bit of refactoring.

--- src/components/molecules/instagram-feed/instagram-feed.js
+++ src/components/molecules/instagram-feed/instagram-feed.js
@@ -5,6 +5,11 @@ var toggleLastTile;
       var $instaText = $('.instafeed-wrapper .instafeed-text').clone();
       $('.instafeed-wrapper .instafeed-text').remove();
       $('.instafeed-images').append($instaText);
+      $('.instafeed-images > a').each(function (i, e) {
+        if (i > 3) {
+          $(e).remove();
+        }
+      });
     };
   });
 }(document, window, jQuery));
