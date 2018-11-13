6b6a7ba398f9b861a9e991e52181e53859820e14
Thiago de Mello Bueno <thiago@madeofpeople.org>
2018-11-13 06:33:04 -0500

    Wraps zoom image calls in conditionals.
    So as not to break stories not containing zoom images.

--- src/components/molecules/zoom-image--fullbleed/zoom-image--fullbleed.js
+++ src/components/molecules/zoom-image--fullbleed/zoom-image--fullbleed.js
@@ -4,31 +4,32 @@
 (function (document, window, $, ImageZoom) {
   $(document).ready(function () {
     $(document).foundation();
-    if (Foundation.MediaQuery.atLeast('large')) {
-      var zoomedImages = new ImageZoom('.img-zoom-drag', { maxZoom: 2, backgroundImageColor: '#000' });
-      $('.img-zoom-drag').mousemove(function (e) {
-        var offset = $(this).offset();
-        var finderBox = $(this).closest('.zoom-image--fullbleed').find('.finder-box');
-        var relX = e.pageX - offset.left;
-        var relY = e.pageY - offset.top;
-        var imgWidth = $(this).width();
-        var imgHeight = $(this).height();
-        var percentThroughWidth = (relX / imgWidth * 100) * .5;
-        var percentThroughHeight = (relY / imgHeight * 100)  * .5;
-        var windowWidth = window.innerWidth;
-        var windowHeight = window.innerHeight;
-        finderBox.css({
-          'top': percentThroughHeight + '%',
-          'left': percentThroughWidth + '%',
-          'height': (windowHeight - 64) / 19 + 'px',
-          'width': windowWidth / 19 + 'px'
+    if (document.querySelectorAll('.img-zoom-drag') > 0) {
+      if (Foundation.MediaQuery.atLeast('large')) {
+        var zoomedImages = new ImageZoom('.img-zoom-drag', { maxZoom: 2, backgroundImageColor: '#000' });
+        $('.img-zoom-drag').mousemove(function (e) {
+          var offset = $(this).offset();
+          var finderBox = $(this).closest('.zoom-image--fullbleed').find('.finder-box');
+          var relX = e.pageX - offset.left;
+          var relY = e.pageY - offset.top;
+          var imgWidth = $(this).width();
+          var imgHeight = $(this).height();
+          var percentThroughWidth = (relX / imgWidth * 100) * .5;
+          var percentThroughHeight = (relY / imgHeight * 100)  * .5;
+          var windowWidth = window.innerWidth;
+          var windowHeight = window.innerHeight;
+          finderBox.css({
+            'top': percentThroughHeight + '%',
+            'left': percentThroughWidth + '%',
+            'height': (windowHeight - 64) / 19 + 'px',
+            'width': windowWidth / 19 + 'px'
+          });
         });
-      });
-    } else {
-      // Can't show this section if we're on mobile devices.
-      $('.img-zoom-drag').closest('section.page').css('display', 'none');
-    }
-    if( document.querySelectorAll('.img-zoom-drag').length > 0 ) {
+      }
+      else {
+        // Can't show this section if we're on mobile devices.
+        $('.img-zoom-drag').closest('section.page').css('display', 'none');
+      }
       var zoomedImages = new ImageZoom('.img-zoom-drag', { maxZoom: 3, backgroundImageColor: '#000' });
     }
   });
